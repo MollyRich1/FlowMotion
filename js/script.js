9884c08b1de9660644abc2094faba0930d505a7e
MollyRich1 <131802119+MollyRich1@users.noreply.github.com>
2024-04-22 01:48:10 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,7 +8,11 @@ function openNav() {
   }
 
   // client testimonial slideshows
+
+  window.addEventListener("resize", showDivs(1));
+  
   var slideIndex = 1;
+  const vpWidth = window.innerWidth;
   showDivs(slideIndex);
   
   function plusDivs(n) {
@@ -20,6 +24,9 @@ function openNav() {
   }
   
   function showDivs(n) {
+    const vpWidth = window.innerWidth;
+
+    if(vpWidth < 992){
     var i;
     var x = document.getElementsByClassName("slide");
    
@@ -29,13 +36,67 @@ function openNav() {
       x[i].style.display = "none";  
     }
     
-    x[slideIndex-1].style.display = "grid";  
+    x[slideIndex-1].style.display = "grid"; 
+  }
+
+  else{
+    var i;
+    var x = document.getElementsByClassName("slide");
+   
+    if (n > 3) {slideIndex = 1}
+    if (n < 1) {slideIndex = 3}
+    for (i = 0; i < 3; i++) {
+      x[i].style.display = "none";  
+    }
+    
+    if(slideIndex == 1){
+    x[0].style.display = "grid"; 
+    x[0].style.gridColumn = "1/2";
+
+    x[1].style.display = "grid"; 
+    x[1].style.gridColumn = "2/3";
+
+    x[2].style.display = "none";
+    x[3].style.display = "none";
+    x[4].style.display = "none";
+    x[5].style.display = "none";
+
+    }
+    else if (slideIndex == 2){
+      x[2].style.display = "grid"; 
+      x[2].style.gridColumn = "1/2";
+  
+      x[3].style.display = "grid"; 
+      x[3].style.gridColumn = "2/3";
+
+    x[0].style.display = "none";
+    x[1].style.display = "none";
+    x[4].style.display = "none";
+    x[5].style.display = "none";
+    }
+    else if (slideIndex == 3){
+      x[4].style.display = "grid"; 
+      x[4].style.gridColumn = "1/2";
+  
+      x[5].style.display = "grid"; 
+      x[5].style.gridColumn = "2/3";
+
+    x[0].style.display = "none";
+    x[1].style.display = "none";
+    x[2].style.display = "none";
+    x[3].style.display = "none";
+    }
+
+  }
 
   }
 
 
+  // window.addEventListener("resize", showDivs(1));
 
-  const viewportWidth = window.innerWidth;
+
+
+  
 
   /* 
   using the viewport width, if above certain threshold, change the style of grid-column
